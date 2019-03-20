cc0a0b71487ea5f65be9d730dd11134f185b209e
Fiabane <fiabane@inf.ufsm.br>
2019-03-20 12:23:43 -0300

    alterações

--- LabKNN/knn.py
+++ LabKNN/knn.py
@@ -7,16 +7,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.datasets import load_svmlight_file
+from operator import itemgetter
+from knn_1 import main as fitting
 from sklearn import preprocessing
 import pylab as pl
 
 def main(data):
 
         # loads data
-        print "Loading data..."
+        # print ("Loading data...")
         X_data, y_data = load_svmlight_file(data)
         # splits data
-        print "Spliting data..."
+        # print ("Spliting data...")
         X_train, X_test, y_train, y_test =  train_test_split(X_data, y_data, test_size=0.5, random_state = 5)
 
         X_train = X_train.toarray()
@@ -30,28 +32,47 @@ def main(data):
         # cria um kNN
         neigh = KNeighborsClassifier(n_neighbors=3, metric='euclidean')
 
-        print 'Fitting knn'
+        # print ('Fitting knn')
         neigh.fit(X_train, y_train)
 
         # predicao do classificador
-        print 'Predicting...'
+        # print ('Predicting...')
         y_pred = neigh.predict(X_test)
 
         # mostra o resultado do classificador na base de teste
-        print neigh.score(X_test, y_test)
+        # print (neigh.score(X_test, y_test))
 
         # cria a matriz de confusao
         cm = confusion_matrix(y_test, y_pred)
-        print cm
+        #print (cm)
+        return (neigh.score(X_test, y_test), cm, data)
         
-	pl.matshow(cm)
-	pl.colorbar()
-	pl.show()
+	# pl.matshow(cm)
+	# pl.colorbar()
+	# pl.show()
 
 if __name__ == "__main__":
-        if len(sys.argv) != 2:
-                sys.exit("Use: knn.py <data>")
 
-        main(sys.argv[1])
-
-
+        # if len(sys.argv) != 2:
+        #         sys.exit("Use: knn.py <data>")
+        #
+        # main(sys.argv[1])
+        scores = []
+        for i in range (5, 100, 5):
+                scores.append(main("features"+str(i)+".txt"))
+        ordered_scores = sorted(scores, key=itemgetter(0), reverse=True)
+        # compare_file = open("cm_compare.txt", "w")
+        last = len(ordered_scores) - 1
+        # compare_file.write("Melhor acurácia: "+ str(ordered_scores[0][0])+ "    (Arquivo: "+ str(ordered_scores[0][2])+")")
+        # compare_file.write("\n")
+        # compare_file.write("Melhor Matriz de confusão:\n " + str(ordered_scores[0][1]))
+        # compare_file.write("\n")
+        # compare_file.write("\n")
+        # compare_file.write("Pior acurácia: " + str(ordered_scores[last][0]) + "    (Arquivo: " + str(ordered_scores[last][2]) + ")")
+        # compare_file.write("\n")
+        # compare_file.write("Pior Matriz de confusão:\n " + str(ordered_scores[last][1]))
+        # compare_file.write("\n")
+        # # for i in range(len(ordered_scores)):
+        # #         print(ordered_scores[i][0])
+        # compare_file.close
+        fitting(str(ordered_scores[0][2]))
---
+++ LabKNN/knn_1.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+# -*- encoding: iso-8859-1 -*-
+
+import sys
+import numpy
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.datasets import load_svmlight_file
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RandomizedSearchCV
+from operator import itemgetter
+from sklearn import preprocessing
+import pylab as pl
+
+def main(data):
+
+        # loads data
+        print("Loading data...")
+        X_data, y_data = load_svmlight_file(data)
+
+        best_fits = []
+        # splits data
+        print("Spliting data... ")
+        X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.5, random_state=5)
+
+        X_train = X_train.toarray()
+        X_test = X_test.toarray()
+
+        # fazer a normalizacao dos dados #######
+        # scaler = preprocessing.MinMaxScaler()
+        # X_train = scaler.fit_transform(X_train_dense)
+        # X_test = scaler.fit_transform(X_test_dense)
+
+        # cria um kNN
+        # neigh = KNeighborsClassifier(n_neighbors=k, metric='euclidean')
+
+        # search for an optimal value of K for KNN
+
+        # list of integers 1 to 30
+        # integers we want to try
+        k_range = range(1, 31)
+
+        # list of scores from k_range
+        k_scores = []
+
+        # distance_options = ['euclidean', 'manhattan', 'mahalanobis', 'minkowski']
+        #
+        # param_grid = dict(n_neighbors=k_range)
+        # # grid = RandomizedSearchCV(estimator=KNeighborsClassifier(), param_grid=param_grid, cv=10, refit=distance_options, scoring='accuracy')
+        # # grid.fit(X_train, y_train)
+        #
+        # best_scores = []
+        #
+        # for _ in list(range(20)):
+        #         rand = RandomizedSearchCV(estimator=KNeighborsClassifier(), param_distributions=param_grid, cv=10, refit=distance_options, scoring='accuracy', n_iter=10)
+        #         rand.fit(X_train, y_train)
+        #         best_scores.append(rand.best_score_)
+        #
+        # print(sorted(best_scores, reverse=True))
+
+        # print(grid.best_score_)
+        # print(grid.best_params_)
+        # print(grid.fit_params)
+        # print(grid.refit)
+        # print(grid.cv_results_)
+        # 1. we will loop through reasonable values of k
+        for k in k_range:
+                metrics = ['euclidean', 'manhattan', 'minkowski']
+                # 2. run KNeighborsClassifier with k neighbours        #
+                for metric in metrics:
+                      knn = KNeighborsClassifier(n_neighbors=k, metric=metric)
+                      # 3. obtain cross_val_score for KNeighborsClassifier with k neighbours
+                      scores = cross_val_score(knn, X_train, y_train, cv=10, scoring='accuracy')
+                      # 4. append mean of scores for k neighbors to k_scores list
+                      k_scores.append([scores.mean(), metric, k])        #
+
+        print(sorted(k_scores, key=itemgetter(0), reverse=True))
+
+
+        # print 'Fitting knn'
+        # neigh.fit(X_train, y_train)
+        #
+        # # predicao do classificador
+        # print 'Predicting...'
+        # y_pred = neigh.predict(X_test)
+        #
+        # # mostra o resultado do classificador na base de teste
+        # print neigh.score(X_test, y_test)
+        #
+        # # cria a matriz de confusao
+        # cm = confusion_matrix(y_test, y_pred)
+        # print cm
+        
+	# pl.matshow(cm)
+	# pl.colorbar()
+	# pl.show()
+
+if __name__ == "__main__":
+        if len(sys.argv) != 2:
+                sys.exit("Use: knn.py <data>")
+
+        main(sys.argv[1])
+
+
