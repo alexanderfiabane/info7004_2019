1817263d5bfeb752f431ab1584dd9461ef64fd69
taperinha <fiabane@inf.ufsm.br>
2019-03-29 17:21:59 -0300

    correções

--- Trab01/clf_scores.py
+++ Trab01/clf_scores.py
@@ -1,55 +1,72 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
+import multiprocessing
 import sys
 from sklearn.datasets import load_svmlight_file
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-import random
-import contextlib
-
-def shuffle_file(path):
-
+from sklearn.utils import shuffle
+import numpy as np
+cores = multiprocessing.cpu_count()
 
 
 def main(rep_train, rep_test):
-    shuffle_file(rep_train)
+
     # loads data
     print("Loading data...")
-    X_data_train, y_data_train = load_svmlight_file(rep_train + ".shuffled")
+    X_data_train, y_data_train = load_svmlight_file(rep_train)
     X_data_test, y_data_test = load_svmlight_file(rep_test)
 
-    batch = range(1000, 26000, 1000)
+    X_data_train, y_data_train = shuffle(X_data_train, y_data_train)
+
+    size_file_train = X_data_train.shape[0]
     knn_scores = []
-    NB_scores = []
+    NB_Gaussian_scores = []
     LDA_scores = []
     LR_scores = []
-    for dtrain in batch:
-        Xtrain = X_data_train[:dtrain]
-        Ytrain = y_data_train[:dtrain]
+    for i in range(1000, (size_file_train + 1000), 1000):
+        print("carregando os primeiros %d registros" % i)
+        Xtrain = X_data_train[:i]
+        Ytrain = y_data_train[:i]
+
+        X = Xtrain.toarray()
+        Y = Ytrain
+        Xtest = X_data_test.toarray()
+
         # lista de classificadores
         # kNN
-        knn = KNeighborsClassifier(n_neighbors=9, metric='euclidean')
-        knn.fit(Xtrain, Ytrain)
-        knn_scores.append([knn.score(X_data_test, y_data_test), dtrain])
+        # knn = KNeighborsClassifier(n_neighbors=9, metric='euclidean', n_jobs=cores)
+        # knn.fit(Xtrain, Ytrain)
+        # knn_scores.append([knn.score(Xtest, y_data_test), i])
 
         # Naïve Bayes
+
+        # Gaussian
         gnb = GaussianNB()
-        gnb.fit(Xtrain, Ytrain)
-        test_pred = gnb.predict(X_data_test)
-        NB_scores.append([accuracy_score(y_data_test, test_pred, normalize=True)])
+        gnb.fit(X, Y)
+        NB_Gaussian_scores.append([gnb.score(Xtest, y_data_test), i])
 
         # LDA
-        # lda = LDA(n_components=2)
-        # lda.fit(Xtrain, Ytrain)
+        lda = LDA()
+        lda.fit(X, Y)
+        LDA_scores.append([lda.score(Xtest, y_data_test), i])
 
-        # LDA_scores.append([knn.score(X_test, y_test), dtrain])
         # Logistic Regression
-        # LR_scores.append([knn.score(X_test, y_test), dtrain])
-    print(knn_scores)
-    print(NB_scores)
+        lr = LogisticRegression(solver='liblinear')
+        lr.fit(X, Y)
+        LR_scores.append([lr.score(Xtest, y_data_test), i])
+
+    # print(knn_scores)
+    print("Imprimindo Naive Bayes...")
+    print("Gaussian...")
+    print(NB_Gaussian_scores)
+    print("LDA...")
+    print(LDA_scores)
+    print("LogisticRegression...")
+    print(LR_scores)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
